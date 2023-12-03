5681799a1507c82bf937618420261d45f5cfaf7f
sifatUap <sifatshaharear.uapbd@gmail.com>
2023-12-03 23:07:00 +0600

    Multiple reducer practice

--- tutorial 2/index.js
+++ tutorial 2/index.js
@@ -1,12 +1,19 @@
-const {createStore} = require ("redux");
+const {createStore, combineReducers} = require ("redux");
 
 const ADD_USER = "ADD_USER";
+const GET_DEPT = "GET_DEPT";
+const ADD_DEPARTMENT = "ADD_DEPARTMENT";
 
 //create state
 
-const initialState = {
+const initialUserState = {
     user: ['Sifat'],
     count: 1,
+}
+
+const initialDeptState = {
+    dept: ['ICT','Exam'],
+    count: 2,
 }
 
 // create action 
@@ -18,25 +25,64 @@ const addUser = (user) => {
     }
 }
 
+const getDept = () => {
+    return {
+        type:GET_DEPT,
+    }
+}
+
+const addDept = (dept) => {
+    return {
+        type:ADD_DEPARTMENT,
+        payload:dept,
+    }
+}
+
+
 // Create reducer
 
-const userReducer = (state = initialState,action) => {
+const userReducer = (state = initialUserState,action) => {
 switch (action.type) {
     case ADD_USER:
         return {
             user: [...state.user, action.payload],
             count: state.count+ 1,
-        }
-        break;
+        };
 
     default:
-        state;
+       return state;
 }
 }
 
+const departmentReducer = (state = initialDeptState,action) => {
+    switch (action.type) {
+        case GET_DEPT:
+            return {
+                dept: [...state.dept],
+                count: state.count,
+            };
+
+            case ADD_DEPARTMENT:
+                return {
+                    dept: [...state.dept,action.payload],
+                    count: state.count+1,
+                };
+        
+    
+        default:
+            return state;
+    }
+    }
+
+    //combine reducer
+
+const mainReducer = combineReducers({
+    departmentR: departmentReducer,
+    userR: userReducer,
+});
 //create store 
 
-const store = createStore(userReducer);
+const store = createStore(mainReducer);
 
 store.subscribe(()=> {
     console.log(store.getState());
@@ -45,3 +91,5 @@ store.subscribe(()=> {
 
 store.dispatch(addUser("shaharear"));
 store.dispatch(addUser("Afran"));
+store.dispatch(getDept());
+store.dispatch(addDept("SSS"));
